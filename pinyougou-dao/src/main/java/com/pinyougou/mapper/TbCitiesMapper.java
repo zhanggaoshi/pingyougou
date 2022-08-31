57fe91e16629ff7fad7c0dd4311a10db9528db0d
wangzhi <980951187@qq.com>
2022-08-31 18:27:01 +0800

    提交pinyougou-dao

---
+++ pinyougou-dao/src/main/java/com/pinyougou/mapper/TbCitiesMapper.java
@@ -0,0 +1,32 @@
+package com.pinyougou.mapper;
+
+
+import org.apache.ibatis.annotations.Param;
+import com.pinyougou.pojo.TbCities;
+import com.pinyougou.pojo.TbCitiesExample;
+
+import java.util.List;
+
+public interface TbCitiesMapper {
+    int countByExample(TbCitiesExample example);
+
+    int deleteByExample(TbCitiesExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TbCities record);
+
+    int insertSelective(TbCities record);
+
+    List<TbCities> selectByExample(TbCitiesExample example);
+
+    TbCities selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByExample(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByPrimaryKeySelective(TbCities record);
+
+    int updateByPrimaryKey(TbCities record);
+}
